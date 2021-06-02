fa9700ce479ed5e544eeb0891f625bc0cffadf60
MohdHoiruttmim <hacker.hoiruttamim@gmail.com>
2021-06-02 14:34:07 +0700

    v1.2.1

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const expand = document.querySelectorAll(".rounded");
 const socBtn = expand[2];
+const nav = document.querySelector(".title");
+const navItem = document.getElementById("navbar");
 
 socBtn.addEventListener('click', () => {
     for(let i=0; i<3; i++){
         expand[i].classList.toggle('active');
     }
+});
+
+nav.addEventListener('click', () =>{
+    navItem.classList.toggle('active');
 })
+
