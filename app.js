b9fa19d4557df2a9a31088aab763a9119bb086c2
MohdHoiruttmim <hacker.hoiruttamim@gmail.com>
2021-06-04 09:42:39 +0700

    v1.2.1

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 const expand = document.querySelectorAll(".rounded");
-const socBtn = expand[2];
 const nav = document.querySelector(".title");
 const navItem = document.getElementById("navbar");
 const icon = document.querySelector(".title i");
 
-socBtn.addEventListener('click', () => {
-    for(let i=0; i<3; i++){
-        expand[i].classList.toggle('active');
-    }
-});
-
-nav.addEventListener('click', () =>{
+icon.addEventListener('click', () =>{
     navItem.classList.toggle('resActive');
     icon.classList.toggle("fi-rr-cross");
     icon.classList.toggle("fi-rr-align-left");
